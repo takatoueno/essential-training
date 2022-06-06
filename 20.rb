182f72ae5c6ecc0f3158c6a6b57a11da006528ce
takatoueno <106798764+takatoueno@users.noreply.github.com>
2022-06-06 05:39:23 +0000

    hoge

--- 20.rb
+++ 20.rb
@@ -1,6 +1,7 @@
-fruit_piece  = {'りんご' => 100 ,'ぶどう' => 200 ,'みかん' => 300}
+fruit_piece = {'りんご' => 100 ,'ぶどう' => 200 ,'みかん' => 300}
 
 fruit_piece.each do |key, value|
   puts "#{key} : #{value}"
+
 end
 
